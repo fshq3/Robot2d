4797699d35dfd6e02191f122c009c857ede8e9b6
fshq3 <fshq3@163.com>
2012-02-24 21:14:45 +0800

    简单xml支持

--- Robot2D/robot2d_world.cpp
+++ Robot2D/robot2d_world.cpp
@@ -2,7 +2,7 @@
 #include <tinyxml/tinyxml.h>
 #include <iostream>
 using namespace std;
-class World
+class c_xml2world
 {
 public:
 	int newId()
@@ -175,11 +175,10 @@ public:
 		}
 		return body;
 	}
-	World()
+	c_xml2world(TiXmlDocument & doc)
 	{
 		b2Vec2 gravity(0.0f, -10.0f);
 		m_world = new b2World(gravity);
-		TiXmlDocument doc( "Robot2DTest.xml" );
 		bool loadOkay = doc.LoadFile();
 		if(!loadOkay)
 		{
@@ -233,53 +232,9 @@ public:
 
 void c_robot2d_world::create()
 {
-	// Define the gravity vector.
-	b2Vec2 gravity(0.0f, -10.0f);
-
-	// Construct a world object, which will hold and simulate the rigid bodies.
-	m_world = new b2World(gravity);
-
-	// Define the ground body.
-	b2BodyDef groundBodyDef;
-	groundBodyDef.position.Set(0.0f, -10.0f);
-
-	// Call the body factory which allocates memory for the ground body
-	// from a pool and creates the ground box shape (also from a pool).
-	// The body is also added to the world.
-	b2Body* groundBody = m_world->CreateBody(&groundBodyDef);
-
-	// Define the ground box shape.
-	b2PolygonShape groundBox;
-
-	// The extents are the half-widths of the box.
-	groundBox.SetAsBox(50.0f, 10.0f);
-
-	// Add the ground fixture to the ground body.
-	groundBody->CreateFixture(&groundBox, 0.0f);
-
-	// Define the dynamic body. We set its position and call the body factory.
-	b2BodyDef bodyDef;
-	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(0.0f, 14.0f);
-	b2Body* body = m_world->CreateBody(&bodyDef);
-
-	// Define another box shape for our dynamic body.
-	b2PolygonShape dynamicBox;
-	dynamicBox.SetAsBox(1.0f, 1.0f);
-
-	// Define the dynamic body fixture.
-	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;
-
-	// Set the box density to be non-zero, so it will be dynamic.
-	fixtureDef.density = 1.0f;
-
-	// Override the default friction.
-	fixtureDef.friction = 0.99f;
-	fixtureDef.restitution = 0.99f;
-	// Add the shape to the body.
-	body->CreateFixture(&fixtureDef);
-
+	TiXmlDocument doc( "Robot2dTest.xml");
+	c_xml2world xml_world(doc);
+	m_world = xml_world.m_world;
 }
 void c_robot2d_world::step(float32 tick_interval)
 {
